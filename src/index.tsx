5d9bcdf07a668b177a59692506786e69eec51f0a
Abi Hafshin <abi@hafs.in>
2020-02-18 15:14:15 +0700

    export useStoreManager

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 export { createAction } from './action';
 export { arrayComparator, strictComparator } from './comparator';
 export { useAction, useDispatcher } from './dispatcher';
-export { StoreProvider } from './provider';
+export { GlobalStates, StoreProvider, useStoreManager } from './provider';
 export { createStore } from './store';
 export { combineStore, useStore } from './useStore';
