9c86d438a25a1d5bf23aba98f3e233061b332b96
Abi Hafshin <abi@hafs.in>
2020-02-21 21:37:59 +0700

    example: don't use anonymous func

--- example/App.tsx
+++ example/App.tsx
@@ -5,18 +5,20 @@ import { Counter } from './Counter';
 import { NameForm } from './NameForm';
 import { Combined } from './Combined';
 
-export const App = () => (
-  <React.StrictMode>
-    <StoreProvider enableDevTool={true}>
-      <Counter />
-      <hr />
-      <NameForm />
-      <hr />
-      <Combined />
-      <hr />
-      <a href="https://github.com/abihf/el-state" target="_blank" rel="noopener noreferrer">
-        GitHub
-      </a>
-    </StoreProvider>
-  </React.StrictMode>
-);
+export function App() {
+  return (
+    <React.StrictMode>
+      <StoreProvider enableDevTool={true}>
+        <Counter />
+        <hr />
+        <NameForm />
+        <hr />
+        <Combined />
+        <hr />
+        <a href="https://github.com/abihf/el-state" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </StoreProvider>
+    </React.StrictMode>
+  );
+}
